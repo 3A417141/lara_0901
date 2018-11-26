ea46a909e936e3e17f95ecf8354226c5d2cf3896
3A417141 <as05202005@gmail.com>
2018-11-26 14:52:08 +0800

    練習5-1 將表單資料利用Model寫入DB

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -1,40 +1,31 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use App\Post;
+
 use App\Http\Requests;
+use App\Post;
+
 class AdminPostsController extends Controller
 {
-public function index()
-{
-    $posts=Post::orderBy('created_at','DESC')->get();
-    $data=['posts'=>$posts];
-    return view('admin.posts.index',$data);
-}
-public function store(Request $request)
-{
-    Post::create($request->all());
-    return redirect()->route('admin.posts.index');
-}
-public function create()
+    public function index()
+    {
+        $posts= Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+        return view('admin.posts.index',$data);
+    }
+	
+    public function create()
     {
         return view('admin.posts.create');
     }
+    public function store(Request $request)
+   {
+       Post::create($request->all());
+   }
     public function edit($id)
     {
-        $post=Post::find($id);
-        $data=['post'=>$post];
-        return view('admin.posts.edit',$data);
+        $data = ['id' => $id];
+        return view('admin.posts.edit', $data);
     }
-public function update(Request $request,$id)
-{
-    $post=Post::find($id);
-    $post->update($request->all());
-    return redirect()->route('admin.posts.index');
 }
-public function store(Request $request){
-        Post::create($request->all());
-        return redirect()->route('admin.posts.index');
-    }
-
- }
