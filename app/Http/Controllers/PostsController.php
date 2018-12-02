a7697741d8ea83028409c3a72632f4e0df0d1a5a
3A417141 <as05202005@gmail.com>
2018-12-02 17:32:41 +0800

    練習2 設定Form Request

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,18 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use App\Http\Requests\PostRequest;
 class PostsController extends Controller
 {
     public function index()
     {
         return view('posts.index');
     }
-
     public function show($id)
     {
         $data = ['id' => $id];
-
         return view('posts.show', $data);
+    }
+    public function store(PostReqest $request){
+    }
+    public function update(PostReqest $request){
+        
     }
 }
